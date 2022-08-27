ff7400958b3a81e8d362bb5018895eb4298a1cbb
Gourang <gorangshrm3@gmail.com>
2022-08-27 09:58:06 +0530

    Assignment 5

---
+++ Assignment - 5  More on Iterative Control Statements/1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    while(n--){
+        printf("MySirG\n");
+    }
+    return 0;
+}
